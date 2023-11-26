102d65cbc997c8e95742d404cd47bcc3e4645878
thanh phú <htp1912@gmail.com>
2023-11-26 14:49:16 +0700

    init

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	key := []byte("1234567890123456") // 32 bytes or 16 bytes
+	plaintext := []byte("some really really really long plaintext")
+	fmt.Printf("%s\n", plaintext)
+	ciphertext, err := encrypt(key, plaintext)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Printf("%0x\n", ciphertext)
+	writeFile("ciphertext.txt", string(ciphertext))
+	result, err := decrypt(key, ciphertext)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Printf("%s\n", result)
+	writeFile("decrypt.patch", string(result))
+}
+
+func writeFile(filename string, content string) error {
+	// Open the file for writing, create it if it doesn't exist, truncate it if it does
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Write the content to the file
+	_, err = file.WriteString(content)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Content successfully written to %s\n", filename)
+	return nil
+}
+
+// See alternate IV creation from ciphertext below
+//var iv = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
+
+func encrypt(key, text []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	b := base64.StdEncoding.EncodeToString(text)
+	ciphertext := make([]byte, aes.BlockSize+len(b))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(b))
+	return ciphertext, nil
+}
+
+func decrypt(key, text []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	if len(text) < aes.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+	iv := text[:aes.BlockSize]
+	text = text[aes.BlockSize:]
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(text, text)
+	data, err := base64.StdEncoding.DecodeString(string(text))
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
